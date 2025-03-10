70280dacd3aa9a89616ba87a5d5af08ce978fd0b
Hyun <105445917+Kwon-Hyun@users.noreply.github.com>
2025-03-10 09:20:06 +0000

    Update human_sound.py

--- vision/human_sound.py
+++ vision/human_sound.py
@@ -5,34 +5,39 @@ from ultralytics import YOLO
 from ultralytics.utils.checks import check_yaml
 from ultralytics.utils import yaml_load
 import time
+import os
+from picamera2 import Picamera2
 
-# parameters
-WIDTH = 1280
-HEIGHT = 720
-model = YOLO('model/yolov8n.pt')
+# 모델 및 오디오 파일 경로 설정
+model_path = os.path.join("model/yolov8n.pt")
+mp3_path = os.path.join("audio/OHT-tts1.mp3")
+
+# YOLO 모델 로드
+model = YOLO(model_path)
 CLASSES = yaml_load(check_yaml('coco128.yaml'))['names']
 colors = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-# 오디오 설정 (MP3 파일 상대경로 지정)
-MP3_FILE = "audio/alert-ex.mp3"  # 사용할 MP3 파일 이름
+# 오디오 설정
 pygame.mixer.init()
 
-# 웹캠 사용
-cap = cv2.VideoCapture(0)  # 0번 카메라 (기본 웹캠)
-cap.set(3, WIDTH)
-cap.set(4, HEIGHT)
+# 카메라 초기화
+picam2 = Picamera2()
+config = picam2.create_preview_configuration(main={"size": (1280, 720), "format": "RGB888"})
+picam2.configure(config)
+picam2.start()
 
 # 사람 감지 여부 확인용 변수
 last_play_time = 0  # 마지막으로 음악을 재생한 시간
 
-# Streaming loop
 try:
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("웹캠에서 프레임을 가져올 수 없습니다.")
-            break
-
+        # Picamera2에서 프레임 가져오기
+        try:
+            frame = picam2.capture_array()
+            
+        except Exception as e:
+            print(f"Frame Error.. : {e}")
+        
         # YOLO 모델 실행
         results = model(frame, stream=True)
 
@@ -71,16 +76,19 @@ try:
         # 사람이 감지되었고, 최근 7초 이내에 음악이 재생되지 않았다면 실행
         current_time = time.time()
         if person_detected and (current_time - last_play_time > 7):
-            pygame.mixer.music.load(MP3_FILE)  # MP3 파일 로드
+            pygame.mixer.music.load(mp3_path)  # MP3 파일 로드
             pygame.mixer.music.play()  # 부저 sound 재생
             last_play_time = current_time  # 마지막 재생 시간 업데이트
 
-        cv2.imshow("Human Sound Test", frame)
+        # 화면 출력
+        cv2.imshow("Human Detection", frame)
 
+        # 종료 조건
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q') or key == 27:
             break
+
 finally:
-    cap.release()
+    picam2.stop()
     cv2.destroyAllWindows()
     pygame.mixer.quit()  # pygame 종료
